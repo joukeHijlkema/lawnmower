07b8ce8c0b993b86e4629c4fe4ef1c371afedef5
joukeHijlkema <hylkema@free.fr>
2016-01-30 18:28:36 +0100

    fixed sync

--- C++/sync.sh
+++ C++/sync.sh
@@ -2,10 +2,11 @@
 ## C++
 mkdir -p Grasmaaier/Clients/C++
 mkdir -p Grasmaaier/Clients/Python
-rsync -r --update --exclude=*.o hylkema@$1:/home/hylkema/Dropbox/Programs/Grasmaaier/C++/Sources /home/pi/Grasmaaier/Clients/C++
-rsync -r hylkema@$1:/home/hylkema/Dropbox/Programs/Grasmaaier/C++/Tests /home/pi/Grasmaaier
-rsync hylkema@$1:/home/hylkema/Dropbox/Programs/Grasmaaier/C++/Makefile /home/pi/Grasmaaier/Clients/C++/Makefile
-rsync hylkema@$1:/home/hylkema/Dropbox/Programs/Grasmaaier/C++/*.sh /home/pi/Grasmaaier/Clients/C++
-rsync hylkema@$1:/home/hylkema/Dropbox/Programs/Grasmaaier/C++/sync.sh /home/pi
+ROOT_DIR=/home/hylkema/Programs/lawnmower
+rsync -r --update --exclude=*.o hylkema@$1:$ROOT_DIR/C++/Sources /home/pi/Grasmaaier/Clients/C++
+rsync -r hylkema@$1:$ROOT_DIR/C++/Tests /home/pi/Grasmaaier
+rsync hylkema@$1:$ROOT_DIR/C++/Makefile /home/pi/Grasmaaier/Clients/C++/Makefile
+rsync hylkema@$1:$ROOT_DIR/C++/*.sh /home/pi/Grasmaaier/Clients/C++
+rsync hylkema@$1:$ROOT_DIR/C++/sync.sh /home/pi
 ## Python
-rsync -r --update --exclude=*.o hylkema@$1:/home/hylkema/Dropbox/Programs/Grasmaaier/Python/* /home/pi/Grasmaaier/Clients/Python
+rsync -r --update --exclude=*.o hylkema@$1:$ROOT_DIR/Python/* /home/pi/Grasmaaier/Clients/Python
